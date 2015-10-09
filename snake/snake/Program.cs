e66b40713efa8cbd58550f74abaa71e64d463eef
Airat Salahov <just.airat@mail.ru>
2015-10-09 17:43:42 +0300

    update

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,7 +10,16 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
             Console.ReadLine();
         }
     }
